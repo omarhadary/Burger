8f87a67f6d2162ec975771c21d480709f73e4e13
Omar Elhadary <Omar Elhadary>
2017-01-21 23:20:32 -0800

    db setup complete

---
+++ config/orm.js
@@ -0,0 +1,37 @@
+var connection = require("../config/connection.js");
+
+// Helper function for SQL syntax.
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
+// Helper function for SQL syntax.
+function objToSql(ob) {
+  var arr = [];
+
+  for (var key in ob) {
+    if (Object.hasOwnProperty.call(ob, key)) {
+      arr.push(key + "=" + ob[key]);
+    }
+  }
+
+  return arr.toString();
+}
+
+var orm = {
+    all: function(tableInput, cb) {
+        var queryString = "SELECT * FROM" + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+}
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES("Double Double", false);
+INSERT INTO burgers (burger_name, devoured) VALUES("The Special", false);
+INSERT INTO burgers (burger_name, devoured) VALUES("My Favourite", false);
