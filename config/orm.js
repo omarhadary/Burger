ffd0390c109aef234300ef2e37ab1e351bbc3193
Omar Elhadary <Omar Elhadary>
2017-01-22 01:02:55 -0800

    controller working, created handlebars and assets files

--- config/orm.js
+++ config/orm.js
@@ -54,7 +54,7 @@ var orm = {
       cb(result);
     });
   },
-  // An example of objColVals would be {name: panther, sleepy: true}
+  
   updateOne: function(table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
